8575a11d760e77efca6040ab6e2723c4d7dda141
Foxpeet <diegobal007@gmail.com>
2022-07-25 12:15:14 +0200

    temporizador para y continua correctamente respetando el tiempo

--- contador.js
+++ contador.js
@@ -21,20 +21,34 @@ function resetContador(){
 
 // Temporizador -----------------------------------------------
 var tiempoInicial = 0
+var tiempoParado = 0
 var activoTemp = 0
+var tiempoaRestar1 = 0
+var tiempoaRestar2 = 0
+var tiempoaRestarTotal = 0
+
 function startTemporizador(){
     if(activoTemp == 0){
+        activoTemp = 1
         if(tiempoInicial == 0){
             tiempoInicial = (new Date()).getTime()
+            document.querySelector("#comenzar").style.display = "none";
+            document.querySelector("#continuar").style.display = "inline";
+            ObtTiempo()
         }
-        activoTemp = 1
-        ObtTiempo()
     }
 }
 
+function continuarTemporizador(){
+    tiempoaRestar2 = (new Date()).getTime()
+    tiempoaRestarTotal = tiempoaRestar2 - tiempoaRestar1 + tiempoaRestarTotal
+    activoTemp = 1
+    ObtTiempo()
+}
 
 function stopTemporizador(){
     activoTemp = 0
+    tiempoaRestar1 = (new Date()).getTime()
 }
 
 function resetTemporizador(){
@@ -42,11 +56,13 @@ function resetTemporizador(){
     tiempoInicial = 0
     activoTemp = 0
     tiempoactivo = 0
+    document.querySelector("#comenzar").style.display = "inline";
+    document.querySelector("#continuar").style.display = "none";
 }
 
 function ObtTiempo(){
     if(activoTemp == 1){
-        tiempoactivo = (((new Date()).getTime())-tiempoInicial)
+        tiempoactivo = (((new Date()).getTime())-tiempoInicial)-tiempoaRestarTotal
         escribirEnDiv("#temporizador", ajustarTiempo(tiempoactivo))
         setTimeout(ObtTiempo, 10)
     } else{
@@ -54,7 +70,7 @@ function ObtTiempo(){
     }
 }
 function ajustarTiempo(milisegundos){
-    milisegundos.toFixed(3)
+    milisegundos.toFixed(2)
     let segundos = parseInt((milisegundos / 1000).toFixed(2))
     let minutos = parseInt((segundos / 60).toFixed(2))
     let horas = parseInt((minutos / 60).toFixed(2))
